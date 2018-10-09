46785ca06512cf03e3873341c0860fed7530de80
Dev-liang <857719089@qq.com>
2018-10-09 17:08:08 +0800

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,22 @@
 <?php
-
-namespace App;
+//Laravel 默认生成的用户模型文件
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+//Authenticatable 是授权相关功能的引用
+
 class User extends Authenticatable
 {
+    //消息通知相关功能引用
     use Notifiable;
-
+    
+    //指明要进行数据库交互的数据库表名称
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
-     *
+     * 过滤用户提交的字段 只有包含在该属性中的字段才能够被正常更新
      * @var array
      */
     protected $fillable = [
@@ -20,7 +25,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      * @var array
      */
     protected $hidden = [
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+//数据库的默认迁移文件
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -8,28 +9,32 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * 运行迁移
      * @return void
      */
     public function up()
     {
+        //创建数据库表
         Schema::create('users', function (Blueprint $table) {
+            //定义数据表字段
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();        //用于保存"记住我"的相关信息
+            //创建了一个 created_at 和一个 updated_at 字段，分别用于保存用户的创建时间和更新时间
+            $table->timestamps();   
         });
     }
 
     /**
      * Reverse the migrations.
-     *
+     * 回滚迁移
      * @return void
      */
     public function down()
     {
+        //删除数据库表
         Schema::dropIfExists('users');
     }
 }
